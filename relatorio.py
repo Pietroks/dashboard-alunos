6582b1fbb94b7ccb8046b0147896ae9aa3eb6281
Pietroks <131334815+Pietroks@users.noreply.github.com>
2025-10-17 08:54:13 -0300

    Update relatorio.py

--- relatorio.py
+++ relatorio.py
@@ -105,7 +105,7 @@ if st.session_state["authentication_status"]:
         header {{
             visibility: hidden;
         }}
-        ._profileContainer_gzau3_53 {{
+        ._profileContainer_gzau3_53, ._profileImage_gzau3_78 _darkThemeShadow_gzau3_91, ._link_gzau3_10 {{
             visibility: hidden !important;
         }}
         </style>
@@ -292,6 +292,7 @@ elif st.session_state["authentication_status"] is False:
     st.error('Usuário ou senha incorreta')
 elif st.session_state["authentication_status"] is None:
     st.warning('Por favor, insira seu usuário e senha')
+
 
 
 
