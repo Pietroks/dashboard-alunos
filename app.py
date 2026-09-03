ce2595ba40594cdaff86f41092fd8a66aebcb0e2
Pietroks <pietrokettner@hotmail.com>
2026-09-03 13:54:15 -0300

    att

--- app.py
+++ app.py
@@ -145,7 +145,7 @@ def main():
             with c_busca:
                 termo_ficha = st.text_input(
                     "Digite o Nome ou Matrícula do aluno:",
-                    placeholder="Ex: Pietro ou 17346...",
+                    placeholder="Ex: Jão ou 17346...",
                     key="termo_ficha"
                 ).strip()
             with c_limpar:
